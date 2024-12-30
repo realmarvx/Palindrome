bccbebc2f63207050b960ec1f708a1e444f9425f
realmarvx <110406309+realmarvx@users.noreply.github.com>
2024-12-30 02:48:52 -0600

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,11 @@
+def Palindrome (N):
+  reverse = N[::-1]
+  if N == reverse:
+      print ("True")
+  else :
+      print ("Flase")
+      
+userinput = Palindrome (input ("Input checker"))
+
+
+  
